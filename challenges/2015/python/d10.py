44d0bca9ee29f6ceb375dddd32a6fcad929574d9
balmeidasoares <balmeidasoares@UK186648>
2019-06-07 16:31:38 +0200

    2015 challenge: days 10 and 11 (python)

---
+++ challenges/2015/python/d10.py
@@ -0,0 +1,30 @@
+'''
+
+Advent of Code - 2015
+
+    --- Day 10: Elves Look, Elves Say ---
+
+Released under the MIT License <http://opensource.org/licenses/mit-license.php>
+
+'''
+
+from itertools import groupby
+
+def part1(sequence='3113322113'):
+    for _ in range(40):
+        sequence = ''.join([str(len(list(g))) + str(k) for k, g in groupby(sequence)])
+
+    return len(sequence)
+
+def part2(sequence='3113322113'):
+    for _ in range(50):
+        sequence = ''.join([str(len(list(g))) + str(k) for k, g in groupby(sequence)])
+
+    return len(sequence)
+
+if __name__ == '__main__':
+    with open('../input/d10.txt', mode='r') as f:
+        _input = f.readline()
+
+        print('Part One: {}'.format(part1(_input)))
+        print('Part Two: {}'.format(part2(_input)))
---
+++ challenges/2015/python/d11.py
@@ -0,0 +1,51 @@
+'''
+
+Advent of Code - 2015
+
+    --- Day 11: Corporate Policy ---
+
+Released under the MIT License <http://opensource.org/licenses/mit-license.php>
+
+'''
+
+import re
+
+def is_valid(password):
+    #all substrings of length 3
+    if all(ord(s[2]) - ord(s[1]) != 1 or ord(s[1]) - ord(s[0]) != 1 for s in [password[i:i+3] for i in range(len(password)-2)]):
+        return False
+    #contains any of the invalid letters
+    if any(password[i] in 'iol' for i in range(len(password))):
+        return False
+    #does not contain two pairs of letters
+    if not re.search(r'([a-z])\1.*([a-z])\2', password):
+        return False
+
+    return True
+
+def increment(password):
+    if password[-1] == 'z':
+        return increment(password[:-1]) + 'a'
+    else:
+        return password[:-1] + chr(ord(password[-1])+1)
+
+def part1(password='cqjxjnds'):
+    while not is_valid(password):
+        password = increment(password)
+
+    return password
+
+def part2(password='cqjxjnds'):
+    p = part1(password)
+    p = increment(p)
+    while not is_valid(p):
+        p = increment(p)
+
+    return p
+
+if __name__ == '__main__':
+    with open('../input/d11.txt', mode='r') as f:
+        _input = f.readline()
+
+        print('Part One: {}'.format(part1(_input)))
+        print('Part Two: {}'.format(part2(_input)))
