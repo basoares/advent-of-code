b7caf6e2be1468c210fc0d245f99f342b468f0e6
bsoares00 <bruno.ed@gmail.com>
2023-11-29 22:42:21 +0100

    2015 challenge: python d01 refactor

--- challenges/2015/python/d01.py
+++ challenges/2015/python/d01.py
@@ -6,22 +6,21 @@ Advent of Code - 2015
 
 '''
 
-tr = {'(': 1, ')': -1}
+from utils import *
 
+def parse_input(day):
+     return day_input(day)[0]
+
+@profiler
 def part1(instructions):
-    return sum(tr[i] for i in instructions)
+    return sum({'(': 1, ')': -1}[i] for i in instructions)
 
+@profiler
 def part2(instructions):
-    floor = 0
-    for idx, i in enumerate(instructions):
-        floor += tr[i]
-
-        if floor == -1:
-            return idx+1
+    return list(accumulate({'(': 1, ')': -1}[i] for i in instructions)).index(-1) + 1
 
 if __name__ == '__main__':
-    with open('../input/d01.txt', mode='r') as f:
-        _input = f.readline()
+        data = parse_input('01')
 
-        print('Part One: {}'.format(part1(_input)))
-        print('Part Two: {}'.format(part2(_input)))
+        print('Part One: {}'.format(part1(data)))
+        print('Part Two: {}'.format(part2(data)))
